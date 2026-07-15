be9858df60a8b3bc1d7be744f8a48a9c47c4e8a4
hrijusutradhar84-a11y <hrijusutradhar84@gmail.com>
2026-07-15 11:14:53 +0530

    Initial Arrengement

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    // We use 'className' in React instead of 'class'
+    <div className="bg-gray-900 min-h-screen flex items-center justify-center text-white">
+      <h1 className="text-5xl font-bold text-green-400">
+        Python Quest UI is Ready!
+      </h1>
+    </div>
+  )
+}
+
+export default App
