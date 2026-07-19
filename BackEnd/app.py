ff65ecd27fcf4e6e0e0589eea7f836e47303b3db
hrijusutradhar84-a11y <hrijusutradhar84@gmail.com>
2026-07-19 19:17:35 +0530

    Spine complete

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
 import bcrypt
+import re
 
 app = Flask(__name__)
 # This line tells Flask: "It is okay to accept data from React (port 5173)"
@@ -37,33 +38,42 @@ def receive_login():
     username = data.get('user')
     plain_password = data.get('pass')
 
-    # 3. Hash the password for security
-    password_bytes = plain_password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password_bytes, salt)
-
-    try:
-        # 4. Connect to SQLite and save the user
-        conn = sqlite3.connect('users.db')
-        cursor = conn.cursor()
+    #eligibilty check
+    pattern = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[_@$])[A-Za-z\d_@$]{8,}$"
+    if re.fullmatch(pattern, plain_password):
         
-        # Insert the data into our table
-        cursor.execute(
-            'INSERT INTO accounts (username, password) VALUES (?, ?)', 
-            (username, hashed_password)
-        )
-        
-        conn.commit()
-        conn.close()
+        password_bytes = plain_password.encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password_bytes, salt)
 
-        print(f"Success! Saved new user: {username}")
-        # Send a success message back to React
-        return jsonify({"status": "success", "message": "User registered successfully"}), 200
+        try:
+            # 4. Connect to SQLite and save the user
+            conn = sqlite3.connect('users.db')
+            cursor = conn.cursor()
+            
+            # Insert the data into our table
+            cursor.execute(
+                'INSERT INTO accounts (username, password) VALUES (?, ?)', 
+                (username, hashed_password)
+            )
+            
+            conn.commit()
+            conn.close()
 
-    except sqlite3.IntegrityError:
-        # If the username already exists, SQLite throws an IntegrityError
-        print(f"Failed: User {username} already exists.")
-        return jsonify({"status": "error", "message": "Username already taken"}), 400
+            
+            # Send a success message back to React
+            return jsonify({"status": "success", "message": "User registered successfully"}), 200
+
+        except sqlite3.IntegrityError:
+            # If the username already exists -> an error
+            print(f"Failed: User {username} already exists.")
+            return jsonify({"status": "error", "message": "Username already taken"}), 400
+    else:
+        return jsonify("WEAK PASSWORD"), 400
+
+    
+
+
 
 
 # --- START THE ENGINE ---
--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
-import mImage from './assets/asset1.png' // Make sure your image name matches this!
+import mImage from './assets/asset1.png' 
 import bgImage from './assets/asset2.jpeg'
 function App() {
   // These two "State" variables hold whatever the user types in real-time
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [currentView, setCurrentView] = useState('')
 
   async function sendDataToBackend(e) {
     
@@ -15,7 +16,7 @@ function App() {
     };
 
     try{
-      const response = await fetch('http://127.0.0', {
+      const response = await fetch('http://127.0.0.1', {
         method: 'POST',
         headers: {
           'content-type': 'application/json',
@@ -41,8 +42,8 @@ function App() {
   const hazyCardStyle = {
     width: '50%',
     height: '50%',
-    backgroundColor: 'rgba(255, 255, 255, 0.2)', // Semi-transparent white
-    backdropFilter: 'blur(10px)',                // This creates the hazy effect
+    backgroundColor: 'rgba(255, 255, 255, 0.2)', 
+    backdropFilter: 'blur(10px)',                
     padding: '50px',
     borderRadius: '16px',
     border: '5px solid rgb(0, 255, 242)',
@@ -80,32 +81,145 @@ function App() {
     transition: 'background-color 0.2s ease'
   }
 
+  return Home()
+}
+function Home() {
+  
+  const containerStyle = {
+    backgroundImage: `url(${bgImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    width: '100%'
+  };
+
+  const hazyCardStyle = {
+    width: '50%',
+    height: '50%',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)', 
+    backdropFilter: 'blur(10px)',                
+    padding: '50px',
+    borderRadius: '16px',
+    border: '5px solid rgb(0, 255, 242)',
+    color: '#fff',
+    boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)'
+  };
+  
+  const buttonStyle = {
+    padding: '12px 20px',
+    borderRadius: '10px',
+    border: 'none',
+    backgroundColor: '#007bff',
+    color: '#ffffff',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    width: '100%',
+    marginTop: '20px',
+    transition: 'background-color 0.2s ease'
+  }
+
+    const textBoxStyle = {
+    width: '100%',
+    padding: '12px 16px',
+    borderRadius: '10px',
+    border: '3px solid rgb(0, 247, 255)',
+    backgroundColor: 'rgb(0, 255, 234)',
+    color: '#ffffff',
+    fontSize: '13px',
+    outline: 'none',
+    boxSizing: 'border-box'
+  }
+
   return <div style={containerStyle}>
     <div style={hazyCardStyle}>
-      <form onSubmit={sendDataToBackend}>
-        <input
-          type="text"
-          style={{...textBoxStyle, marginBottom: '20px'}}
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder='Put your user name here'
-        />
-        <input
-          type='password'
-          style={textBoxStyle}
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder='make a strong password'
-        />
-        <button
-          type='submit'
-          style={buttonStyle}
+      <form>
+        <button 
+        style={buttonStyle}
+        type='submit'
         >
           Log In
+        </button>
+        <button style={buttonStyle}
+        type='submit'>
+          Sign Up
         </button>
       </form>
     </div>
   </div>
 }
 
-export default App;
+function logIn () {
+    const containerStyle = {
+    backgroundImage: `url(${bgImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+    width: '100%'
+  };
+
+  const hazyCardStyle = {
+    width: '50%',
+    height: '50%',
+    backgroundColor: 'rgba(255, 255, 255, 0.2)', 
+    backdropFilter: 'blur(10px)',                
+    padding: '50px',
+    borderRadius: '16px',
+    border: '5px solid rgb(0, 255, 242)',
+    color: '#fff',
+    boxShadow: '0 4px 30px rgba(0, 0, 0, 0.1)'
+  };
+  
+  const buttonStyle = {
+    padding: '12px 20px',
+    borderRadius: '10px',
+    border: 'none',
+    backgroundColor: '#007bff',
+    color: '#ffffff',
+    fontSize: '16px',
+    fontWeight: 'bold',
+    cursor: 'pointer',
+    width: '100%',
+    marginTop: '20px',
+    transition: 'background-color 0.2s ease'
+  }
+      const textBoxStyle = {
+    width: '100%',
+    padding: '12px 16px',
+    borderRadius: '10px',
+    border: '3px solid rgb(0, 247, 255)',
+    backgroundColor: 'rgb(0, 255, 234)',
+    color: '#ffffff',
+    fontSize: '13px',
+    outline: 'none',
+    boxSizing: 'border-box'
+  }
+
+
+  return <div style={containerStyle}>
+    <div style={hazyCardStyle}>
+      <input
+        type='password'
+        style={textBoxStyle}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder='give your set password'
+      />
+      <input 
+        type='password'
+        style={textBoxStyle}
+        value={password}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder='give your set password'
+      />
+    </div>
+  </div>
+};
